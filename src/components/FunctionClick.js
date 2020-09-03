ace26119849b161a3ca8699c6ceaa4a8e660426d
tummalavinaya <65774807+tummalavinaya@users.noreply.github.com>
2020-09-03 08:03:01 -0400

    Added log message in this componet

--- src/components/FunctionClick.js
+++ src/components/FunctionClick.js
@@ -2,6 +2,7 @@ import React from 'react';
 function FunctionClick(){
      function clickHandler(){
         console.log("click handler method");
+          console.log("Click handler method");
     }
     return (<div>
         <button onClick = {clickHandler}>click</button>
